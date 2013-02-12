6c06346052af7bacfc05b682cbd089e45a2cc186
David Foster <davidfstr@gmail.com>
2013-02-11 16:31:13 -0800

    Integrate basic Django form to represent app settings.

--- server/apps/setupfx/views.py
+++ server/apps/setupfx/views.py
@@ -1,4 +1,14 @@
+# TODO: Move this section to a more appropriate file and import from here
+from django import forms
+
+class AppSettingsForm(forms.Form):
+    # TODO: Use specialized email field to perform extra validation
+    email = forms.CharField(max_length=100)
+
+# ------------------------------------------------------------------------------
+
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from appfx.decorators.render import render_to
 
 @render_to('setupfx:home.html')
@@ -12,6 +22,19 @@ def home(request):
 @render_to('setupfx:setup.html')
 @login_required
 def setup(request):
+    if request.method == 'POST':
+        form = AppSettingsForm(request.POST)
+        if form.is_valid():
+            # TODO: Persist form data.
+            # TODO: Mark app as setup.
+            print 'setupfx: Received settings: ' + unicode(form.cleaned_data)
+            
+            # Return to home page
+            # TODO: Use reverse function to get correct URL
+            return HttpResponseRedirect('/appfx/setupfx/setup/')
+    else:
+        form = AppSettingsForm()
+    
     return {
-        # No parameters
+        'form': form,
     }
