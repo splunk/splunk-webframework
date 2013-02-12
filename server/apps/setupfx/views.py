2e6ddc593faebfeeecb1e8cdaf56807f6d87335b
David Foster <davidfstr@gmail.com>
2013-02-12 14:15:37 -0800

    Add some more form fields.

--- server/apps/setupfx/views.py
+++ server/apps/setupfx/views.py
@@ -1,8 +1,24 @@
 # TODO: Move this section to a more appropriate file and import from here
+from decimal import Decimal
 from django import forms
 
 class AppSettingsForm(forms.Form):
+    enable_input = forms.BooleanField(
+        label='Enable Flurry data collection',
+        initial=True, required=False)  # boolean fields must be non-required
+    
     email = forms.EmailField(max_length=100)
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput())
+    project_id = forms.IntegerField(max_value=1,
+        label='Project ID',
+        help_text=
+            'The project ID can be obtained by logging in to the Flurry ' +
+            'dashboard, [...]?<b>projectID=<u>12345</u></b>&dashboardId=22')
+    
+    delay_between_requests = forms.DecimalField(
+        initial=Decimal('10.0'), min_value=0.0)
+    delay_after_rate_limited = forms.DecimalField(
+        initial=Decimal('60.0'), min_value=0.0)
 
 # ------------------------------------------------------------------------------
 
